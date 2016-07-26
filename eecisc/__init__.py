fc961fdcafca9b676cbb8503e13c76fb2d835be8
Tim Tröndle <tt397@cam.ac.uk>
2016-07-26 09:00:15 +0100

    Added basic package setup

---
+++ eecisc/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1.0.dev'
+
+IP_ADDRESS = '129.169.70.197'
+COMPUTER_NAME = 'eecisc'
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+exec(open('eecisc/__init__.py').read())
+
+setup(
+    name='eecisc',
+    version=__version__,
+    description='Tools for accessing eecisc.eng.cam.ac.uk.',
+    maintainer='Tim Tröndle',
+    maintainer_email='tt397@cam.ac.uk',
+    url='https://www.github.com/timtroendle/eecisc',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    install_requires=['pysmb'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering'
+    ]
+)
